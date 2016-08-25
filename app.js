8b3d7840c724addc9c6e53fef85d051f48da30e7
Stephen Knutter <stephen.knutter@gmail.com>
2016-08-25 13:31:32 -0600

    setting up server

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var port = process.env.PORT;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
